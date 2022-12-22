0d56c05eb847005cd1c0df1d15d026e3c7a2cdea
williamcsa04 <williamcaetano14@gmail.com>
2022-12-21 21:41:02 -0300

    :heavy_plus_sign: Added fiber

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-func apiResponse(w http.ResponseWriter, r *http.Request) {
-	// Set the return Content-Type as JSON like before
-	w.Header().Set("Content-Type", "application/json")
-
-	// Change the response depending on the method being requested
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "GET method requested"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "POST method requested"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "Can't find method requested"}`))
-	}
-}
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	http.HandleFunc("/", apiResponse)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
